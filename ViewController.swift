f65e9b5ab9c0fe4d55d310669bd1f2046ef631d7
李长松 <lcs_lz@163.com>
2016-06-16 10:50:37 +0800

    no message

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPersist
+//
+//  Created by 李长松 on 16/6/16.
+//  Copyright © 2016年 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
